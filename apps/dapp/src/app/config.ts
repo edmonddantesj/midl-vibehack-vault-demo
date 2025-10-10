0627e0b64c1a3e294ac7194018792cceab38b09d
Alex Novak <heyalexnovak@gmail.com>
2025-10-10 15:45:27 +0100

    feat: use maestro symphone runes provider

--- apps/dapp/src/app/config.ts
+++ apps/dapp/src/app/config.ts
@@ -1,10 +1,11 @@
-import { type Config, regtest } from "@midl/core";
+import { type Config, MaestroSymphonyProvider, regtest } from "@midl/core";
 import { createMidlConfig } from "@midl/satoshi-kit";
 import { QueryClient } from "@tanstack/react-query";
 
 export const midlConfig = createMidlConfig({
 	networks: [regtest],
 	persist: true,
+	runesProvider: new MaestroSymphonyProvider(),
 }) as Config;
 
 export const queryClient = new QueryClient({
